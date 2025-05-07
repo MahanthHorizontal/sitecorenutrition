8b48da1d8dad82d80917ef99b067b04b4689e042
MahanthHorizontal <135674152+MahanthHorizontal@users.noreply.github.com>
2025-05-07 11:31:29 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,23 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL', // Or just omit this header entirely
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors *", // Or restrict to Sitecore: "frame-ancestors 'self' https://your-sitecore-domain.com"
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
